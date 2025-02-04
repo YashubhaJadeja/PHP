72eea9fe7a8e4d1183131c4f4a72507a483e7b6e
Yashubha__13 <146408302+YashubhaJadeja@users.noreply.github.com>
2025-02-04 16:16:34 +0530

    Add files via upload

---
+++ Lab_6/4/about.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title> Page </title>
+        <link href="Style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+
+
+<?php 
+
+include 'header.php'
+
+?>
+
+    <h1>About Us</h1>
+    <p>This is the about page.</p>
+
+<?php 
+    include 'footer.php'
+
+    ?>
+
+    </body>
+</html>
+
---
+++ Lab_6/4/contact.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head>
+        <title> Page </title>
+        <link href="Style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+<?php include 'header.php'; ?>
+
+    <h1>Contact Us</h1>
+    <p>This is the contact page.</p>
+
+<?php 
+    include 'footer.php'
+     ?>
+
+
+    </body>
+</html>
